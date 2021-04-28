e65fb9055b24d040436096e11ae6dfe19a4839c0
Doireann-Ni-Ghrioghair <doireanndoireann@gmail.com>
2021-04-28 20:33:55 +0100

    Chore:write Readme file

--- src/App.js
+++ src/App.js
@@ -11,14 +11,6 @@ class BooksApp extends React.Component {
     books:[],
     screen:'bookshelf',
 
-    //query:""
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-   //showSearchPage: false
   }
 
   
@@ -62,39 +54,7 @@ class BooksApp extends React.Component {
     )
   }
 }
-     /* <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                *///}
-               /* 
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
-         
-                            
-                           
-          
-            <div className="open-search">
-              <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
-            </div>
-          </div>
-        )}
-      </div>*/
+     
 
 
 export default BooksApp
